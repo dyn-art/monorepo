b91f36e0ab8bc8b6e0218822afe7cbb33e56d234
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2023-11-03 13:20:34 +0100

    #develop use absolute script path

--- scripts/prepare_rust.sh
+++ scripts/prepare_rust.sh
@@ -5,11 +5,12 @@ set -e # Exit immediately if a command exits with a non-zero status.
 echo "Preparing Rust environment..."
 
 # Make sure the scripts are executable
-chmod +x install_rust.sh
-chmod +x install_wasm-pack.sh
+DIR="$(dirname "$0")"
+chmod +x "$DIR/install_rust.sh"
+chmod +x "$DIR/install_wasm-pack.sh"
 
 # Execute the scripts
-./install_rust.sh
-./install_wasm-pack.sh
+"$DIR/install_rust.sh"
+"$DIR/install_wasm-pack.sh"
 
 echo "Rust environment is ready."
