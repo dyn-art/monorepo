95e461775dd0448b8abf59fdc32747e94e2f5b3a
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2023-11-03 16:39:03 +0100

    #develop setup specific Rust env only on Vercel

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -2,19 +2,23 @@
 
 # This script serves as a local execution wrapper for the dyn-cli tool.
 # We are using this approach because we couldn't figure out how to
-# reliable bind the "dyn-cli" command in the local development environment 
+# reliable bind the "dyn-cli" command in the local development environment
 # and within the constraints of a PaaS build step.
-# It ensures the run.sh script within the CLI's bin directory is directly invoked 
-# with the necessary arguments.
+# It ensures the run.sh script within the CLI's bin directory is directly invoked
+# with the necessary arguments and environment variables setup.
 
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Add Rust's cargo bin directory to PATH
-export PATH="/root/.cargo/bin:$PATH"
-
-# Set the default Rust toolchain to stable
-rustup default stable
+# Check if we are running on Vercel (https://vercel.com/docs/projects/environment-variables/system-environment-variables)
+if [ "$VERCEL" = "1" ]; then
+    echo "Detected Vercel environment. Setting up..."
+    DIR="$(dirname "$0")"
+    chmod +x "$DIR/vercel/cli_setup.sh"
+    source "$DIR/vercel/cli_setup.sh"
+else
+    echo "Running locally..."
+fi
 
 # Find the absolute path to the monorepo's root directory
 MONOREPO_ROOT=$(git rev-parse --show-toplevel)
--- scripts/vercel/install_rust.sh
+++ scripts/vercel/install_rust.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Exit immediately if a command exits with a non-zero status
-set -e 
+set -e
 
 echo "Installing Rustup..."
 
