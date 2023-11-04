e5628f49feb6110f97c5e011157a220c10cdc322
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2023-11-04 12:10:15 +0100

    #9 fixed typo

--- scripts/vercel/setup_rust.sh
+++ scripts/vercel/setup_rust.sh
@@ -6,7 +6,8 @@ set -e
 echo "🦀 Preparing Rust environment..."
 
 DIR="$(dirname "$0")"
-RUST_ENV_FILE="$HOME/temp/rust/env.sh"
+RUST_ENV_DIR="$HOME/temp/rust"
+RUST_ENV_FILE="$RUST_ENV_DIR/env.sh"
 
 # Execute the scripts and source them to ensure environment changes affect the current shell
 chmod +x "$DIR/install_rust.sh"
@@ -14,10 +15,13 @@ source "$DIR/install_rust.sh"
 chmod +x "$DIR/install_wasm-pack.sh"
 source "$DIR/install_wasm-pack.sh"
 
+# Ensure the Rust environment directory exists
+mkdir -p "$RUST_ENV_DIR"
+
 # Write the Rust-specific environment variables into a file
 {
     echo "export HOME=\"$HOME\""
-    echo "RUST_PROFILE=\"$HOME/.cargo/env\""
+    echo "export RUST_PROFILE=\"$HOME/.cargo/env\""
 } >"$RUST_ENV_FILE"
 
 # Append a command to source the Rust profile to the file
