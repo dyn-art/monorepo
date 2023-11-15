50a5af30ebe6fdae2dd7498a1581b9dc123713c1
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2023-11-15 18:00:30 +0100

    #9 improved ordering changes system

--- packages/svg-composition/rust/src/core/modules/svg_render/systems.rs
+++ packages/svg-composition/rust/src/core/modules/svg_render/systems.rs
@@ -25,7 +25,7 @@ pub fn extract_mixin_generic<T: Component + ToMixinChange>(
 ) {
     query.for_each(|(entity, node, mixin)| {
         let changed_component = changed.changes.entry(entity).or_insert_with(|| {
-            // Attempt to get the parent entity id
+            // Try to get the parent entity id
             let mut parent_id: Option<Entity> = None;
             if let Ok(parent) = parent_query.get(entity) {
                 parent_id = Some(parent.get());
@@ -42,52 +42,34 @@ pub fn extract_mixin_generic<T: Component + ToMixinChange>(
 }
 
 pub fn queue_render_changes(
-    mut changed: ResMut<ChangedComponents>,
+    changed: ResMut<ChangedComponents>,
     mut svg_composition: ResMut<SVGComposition>,
 ) {
-    // Collect entities to process
-    let to_process = initiate_entity_collection(&changed.changes);
-
-    // Process each collected entity
-    for entity in to_process {
-        if let Some(changed_component) = changed.changes.get(&entity) {
-            process_entity(entity, changed_component, &mut svg_composition);
-        }
-    }
-}
-
-fn initiate_entity_collection(changes: &HashMap<Entity, ChangedComponent>) -> Vec<Entity> {
-    let mut to_process = HashSet::new();
+    let changes = &changed.changes;
     let mut processed = HashSet::new();
 
-    for &entity in changes.keys() {
-        collect_entities_iteratively(&mut to_process, &mut processed, changes, entity);
+    // Recursive function to process an entity and its parents
+    fn process_with_parents(
+        entity: Entity,
+        changes: &HashMap<Entity, ChangedComponent>,
+        processed: &mut HashSet<Entity>,
+        svg_composition: &mut ResMut<SVGComposition>,
+    ) {
+        if processed.insert(entity) {
+            if let Some(change) = changes.get(&entity) {
+                if let Some(parent_id) = change.parent_id {
+                    // Process parent first
+                    process_with_parents(parent_id, changes, processed, svg_composition);
+                }
+                // Process the current entity
+                process_entity(entity, change, svg_composition);
+            }
+        }
     }
 
-    to_process.into_iter().collect()
-}
-
-fn collect_entities_iteratively(
-    to_process: &mut HashSet<Entity>,
-    processed: &mut HashSet<Entity>,
-    changes: &HashMap<Entity, ChangedComponent>,
-    start_entity: Entity,
-) {
-    let mut stack = vec![start_entity];
-
-    while let Some(entity) = stack.pop() {
-        // Skip already processed entities
-        if !processed.insert(entity) {
-            continue;
-        }
-
-        if let Some(changed) = changes.get(&entity) {
-            if let Some(parent_id) = changed.parent_id {
-                stack.push(parent_id);
-            }
-
-            to_process.insert(entity);
-        }
+    // Iterate over changes and process each entity
+    for &entity in changes.keys() {
+        process_with_parents(entity, changes, &mut processed, &mut svg_composition);
     }
 }
 
