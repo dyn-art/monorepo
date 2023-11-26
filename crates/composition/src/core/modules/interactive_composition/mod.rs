84893590b386191bb578d4dd3d4e282019220a09
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2023-11-26 12:23:30 +0100

    #9 imporved system set ordering

--- crates/composition/src/core/modules/interactive_composition/mod.rs
+++ crates/composition/src/core/modules/interactive_composition/mod.rs
@@ -1,5 +1,5 @@
 use bevy_app::{Plugin, PreUpdate};
-use bevy_ecs::schedule::{IntoSystemConfigs, SystemSet};
+use bevy_ecs::schedule::{IntoSystemConfigs, IntoSystemSetConfigs, SystemSet};
 
 use self::{
     events::{
@@ -42,27 +42,31 @@ impl Plugin for InteractiveCompositionPlugin {
         // Register resources
         app.world.init_resource::<InteractiveCompositionRes>();
 
+        // Configure system sets
+        app.configure_sets(
+            PreUpdate,
+            (
+                InteractionSet::First,
+                InteractionSet::Initial,
+                InteractionSet::Continuous,
+                InteractionSet::Completion,
+                InteractionSet::Last,
+            )
+                .chain(),
+        );
+
         // Register systems
         app.add_systems(
             PreUpdate,
             (
                 handle_cursor_entered_composition.in_set(InteractionSet::First),
-                handle_cursor_down_on_composition
-                    .in_set(InteractionSet::Initial)
-                    .after(InteractionSet::First),
+                handle_cursor_down_on_composition.in_set(InteractionSet::Initial),
                 handle_cursor_down_on_entity_event
                     .in_set(InteractionSet::Initial)
-                    .after(handle_cursor_down_on_composition)
-                    .after(InteractionSet::First),
-                handle_cursor_moved_on_composition
-                    .in_set(InteractionSet::Continuous)
-                    .after(InteractionSet::Initial),
-                handle_cursor_up_on_composition
-                    .in_set(InteractionSet::Completion)
-                    .after(InteractionSet::Continuous),
-                handle_cursor_exited_composition
-                    .in_set(InteractionSet::Last)
-                    .after(InteractionSet::Completion),
+                    .after(handle_cursor_down_on_composition),
+                handle_cursor_moved_on_composition.in_set(InteractionSet::Continuous),
+                handle_cursor_up_on_composition.in_set(InteractionSet::Completion),
+                handle_cursor_exited_composition.in_set(InteractionSet::Last),
             ),
         );
     }
