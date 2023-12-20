61bc1116418f3378b399786a87b40c48927bf9b7
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2023-12-20 10:36:35 +0100

    #27 fixed color

--- packages/figma-to-dtif/src/utils/converter/convert-figma-rgb-to-dtif.ts
+++ packages/figma-to-dtif/src/utils/converter/convert-figma-rgb-to-dtif.ts
@@ -1,3 +1,3 @@
 export function convertFigmaRGBToDTIF(rgb: RGB): [number, number, number] {
-	return [rgb.r, rgb.g, rgb.b];
+	return [rgb.r, rgb.g, rgb.b].map((value) => Math.round(value * 255)) as [number, number, number];
 }
