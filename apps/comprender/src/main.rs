7b2175d33dd84365c75ba41a1605a7d77b145a3a
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-01-06 16:13:25 +0100

    #35 wip

--- apps/comprender/src/main.rs
+++ apps/comprender/src/main.rs
@@ -1,11 +1,36 @@
+use std::{
+    net::{Ipv4Addr, SocketAddr},
+    sync::Arc,
+};
+
 use axum::{routing::get, Router};
+// use rspc::{BuiltRouter, Rspc};
+
+// const R: Rspc<()> = Rspc::new();
+
+// fn router() -> Arc<BuiltRouter> {
+//     R.router()
+//         .procedure("version", R.query(|ctx, _: ()| env!("CARGO_PKG_VERSION")))
+//         .build()
+//         .unwrap()
+//         .arced()
+// }
 
 #[tokio::main]
 async fn main() {
+    // let router = router();
+    // router
+    //     .export_ts(ExportConfig::new(
+    //         PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../bindings.ts"),
+    //     ))
+    //     .unwrap();
+
     // Build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    // .route("/rspc/:id", router.endpoint(|| ()).axum());
 
     // Run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3000));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
