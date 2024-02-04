8f225b1306690b3d946ed6579c3e73cb24202401
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-02-04 17:07:13 +0100

    #develop fixed rotation https://stackoverflow.com/questions/77935364/rotating-a-rectangle-around-its-center-by-dragging-a-corner-with-cursor Thanks @Aurel300 for the hint :)

--- crates/composition/src/modules/interactive_composition/systems/cursor/composition/cursor_move/rotating.rs
+++ crates/composition/src/modules/interactive_composition/systems/cursor/composition/cursor_move/rotating.rs
@@ -38,26 +38,25 @@ pub fn handle_rotating(
 
     selected_nodes_query.for_each_mut(
         |(mut relative_transform_mixin, absolute_transform_mixin, dimension_mixin)| {
-            let relative_pivot_point = Vec2::new(
-                dimension_mixin.width as f32 / 2.0,
-                dimension_mixin.height as f32 / 2.0,
-            );
+            let width: f32 = dimension_mixin.width;
+            let height: f32 = dimension_mixin.height;
+            let relative_pivot_point = Vec2::new(width / 2.0, height / 2.0);
             let absolute_pivot_point =
                 apply_transform_to_point(absolute_transform_mixin.0, relative_pivot_point);
 
             // Determine rotation offset based on corner
             let rotation_offset_in_radians: f32 = match corner {
                 _ if corner == (HandleSide::Top as u8 | HandleSide::Left as u8) => {
-                    (-135.0 as f32).to_radians()
+                    f32::atan2(-height, -width)
                 }
                 _ if corner == (HandleSide::Top as u8 | HandleSide::Right as u8) => {
-                    (-45.0 as f32).to_radians()
+                    f32::atan2(-height, width)
                 }
                 _ if corner == (HandleSide::Bottom as u8 | HandleSide::Right as u8) => {
-                    (45.0 as f32).to_radians()
+                    f32::atan2(height, width)
                 }
                 _ if corner == (HandleSide::Bottom as u8 | HandleSide::Left as u8) => {
-                    (135.0 as f32).to_radians()
+                    f32::atan2(height, -width)
                 }
                 _ => 0.0,
             };
@@ -83,7 +82,10 @@ fn calculate_rotation(
     pivot_point: &Vec2,
 ) -> f32 {
     // Calculate the angle from the pivot point to the current cursor position
-    let current_angle = (cursor_point.y - pivot_point.y).atan2(cursor_point.x - pivot_point.x);
+    let current_angle = f32::atan2(
+        cursor_point.y - pivot_point.y,
+        cursor_point.x - pivot_point.x,
+    );
 
     // Calculate the raw angle difference
     let angle_diff = current_angle - initial_angle_in_radians;
