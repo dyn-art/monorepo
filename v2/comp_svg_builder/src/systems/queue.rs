8228d4a068517b7c62188e4a9773e8ff7f3adc65
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-03-13 07:25:35 +0100

    #48 fixed typos

--- v2/comp_svg_builder/src/systems/queue.rs
+++ v2/comp_svg_builder/src/systems/queue.rs
@@ -6,68 +6,40 @@ use crate::{
         changed_svg_bundles::{ChangedSvgBundle, ChangedSvgBundlesRes},
         output_event_sender::OutputEventSenderRes,
     },
+    svg::svg_element::element_changes::SvgElementChanges,
 };
 use bevy_ecs::{entity::Entity, system::ResMut};
 use std::collections::{HashMap, HashSet, VecDeque};
 
 pub fn queue_svg_bundle_changes(
     mut changed_svg_bundles_res: ResMut<ChangedSvgBundlesRes>,
-    output_event_sender_res: ResMut<OutputEventSenderRes>,
+    mut output_event_sender_res: ResMut<OutputEventSenderRes>,
 ) {
     let changed_bundles = changed_svg_bundles_res.drain_changes();
+    queue_changed_bundles(changed_bundles, &mut output_event_sender_res);
+
     let deferred_elements_changes = changed_svg_bundles_res.drain_deferred_changes();
+    queue_deferred_elements_changes(deferred_elements_changes, &mut output_event_sender_res);
+}
+
+fn queue_changed_bundles(
+    changed_bundles: Vec<ChangedSvgBundle>,
+    output_event_sender_res: &mut OutputEventSenderRes,
+) {
     let changes_length = changed_bundles.len();
 
-    let entities_set: HashSet<Entity> =
-        changed_bundles.iter().map(|bundle| bundle.entity).collect();
-
-    // Mapping of parent id to children, still maintaining order
-    let mut parent_to_children: HashMap<Option<Entity>, Vec<ChangedSvgBundle>> = HashMap::new();
-
-    // Separate changes into roots (None, that have no parent in the current changes)
-    // and children grouped by parent entities
-    for changed_bundle in changed_bundles {
-        let is_orphan = changed_bundle
-            .parent_entity
-            .map_or(true, |parent| !entities_set.contains(&parent));
-
-        if is_orphan {
-            parent_to_children
-                .entry(None)
-                .or_default()
-                .push(changed_bundle);
-        } else {
-            parent_to_children
-                .entry(changed_bundle.parent_entity)
-                .or_default()
-                .push(changed_bundle);
-        }
-    }
+    // Map children to their parents
+    let mut parent_to_children_map = create_parent_to_children_map(changed_bundles);
 
     // Sort children within their parent grouping by their index
-    for children in parent_to_children.values_mut() {
-        children.sort_by(|a, b| b.index.cmp(&a.index));
+    for children in parent_to_children_map.values_mut() {
+        children.sort_by(|a, b| a.index.cmp(&b.index));
     }
 
-    // Process root nodes in depth-first order
-    let mut sorted_changed_bundles: Vec<ChangedSvgBundle> = Vec::with_capacity(changes_length);
-    let mut root_bundles: VecDeque<ChangedSvgBundle> = parent_to_children
-        .remove(&None)
-        .unwrap_or_else(Vec::new)
-        .into_iter()
-        .collect();
+    // Process root bundles in depth-first order
+    let sorted_changed_bundles = process_root_bundles(&mut parent_to_children_map, changes_length);
 
-    while let Some(change) = root_bundles.pop_back() {
-        if let Some(children) = parent_to_children.remove(&Some(change.entity)) {
-            for child in children.into_iter().rev() {
-                root_bundles.push_back(child);
-            }
-        }
-
-        sorted_changed_bundles.push(change);
-    }
-
-    // Send output events for sorted changes
+    // Send output events
     for changed_bundle in sorted_changed_bundles {
         for element_changes in changed_bundle.elements_changes {
             let event = SvgBuilderOutputEvent::SvgElementChanges(SvgElementChangesOutputEvent {
@@ -76,8 +48,67 @@ pub fn queue_svg_bundle_changes(
             output_event_sender_res.push_event(event);
         }
     }
+}
 
-    // Send output events for deferred changes
+fn create_parent_to_children_map(
+    changed_bundles: Vec<ChangedSvgBundle>,
+) -> HashMap<Option<Entity>, Vec<ChangedSvgBundle>> {
+    let changed_entities_set: HashSet<Entity> =
+        changed_bundles.iter().map(|bundle| bundle.entity).collect();
+    let mut parent_to_children_map: HashMap<Option<Entity>, Vec<ChangedSvgBundle>> = HashMap::new();
+
+    // Separate changes into roots (None, that have no parent in the current changes)
+    // and children grouped by parent entities
+    for changed_bundle in changed_bundles {
+        let is_orphan = changed_bundle
+            .parent_entity
+            .map_or(true, |parent| !changed_entities_set.contains(&parent));
+
+        if is_orphan {
+            parent_to_children_map
+                .entry(None)
+                .or_default()
+                .push(changed_bundle);
+        } else {
+            parent_to_children_map
+                .entry(changed_bundle.parent_entity)
+                .or_default()
+                .push(changed_bundle);
+        }
+    }
+
+    return parent_to_children_map;
+}
+
+fn process_root_bundles(
+    parent_to_children: &mut HashMap<Option<Entity>, Vec<ChangedSvgBundle>>,
+    capacity: usize,
+) -> Vec<ChangedSvgBundle> {
+    let mut sorted_changed_bundles: Vec<ChangedSvgBundle> = Vec::with_capacity(capacity);
+    let mut root_bundles: VecDeque<ChangedSvgBundle> = parent_to_children
+        .remove(&None)
+        .unwrap_or_else(Vec::new)
+        .into_iter()
+        .collect();
+
+    // Process root bundles in depth-first order
+    while let Some(change) = root_bundles.pop_front() {
+        if let Some(children) = parent_to_children.remove(&Some(change.entity)) {
+            for child in children {
+                root_bundles.push_back(child);
+            }
+        }
+        sorted_changed_bundles.push(change);
+    }
+
+    return sorted_changed_bundles;
+}
+
+fn queue_deferred_elements_changes(
+    deferred_elements_changes: Vec<SvgElementChanges>,
+    output_event_sender_res: &mut OutputEventSenderRes,
+) {
+    // Send output events
     for deferred_elements_change in deferred_elements_changes {
         let event = SvgBuilderOutputEvent::SvgElementChanges(SvgElementChangesOutputEvent {
             changes: deferred_elements_change,
