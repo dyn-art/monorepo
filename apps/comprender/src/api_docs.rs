c172a290259136e66757ad90cb1243fe73ca7308
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-03-16 13:03:09 +0100

    #48 deprecated old code

--- apps/comprender/src/api_docs.rs
+++ apps/comprender/src/api_docs.rs
@@ -1,19 +1,19 @@
-use utoipa::OpenApi;
+// use utoipa::OpenApi;
 
-#[derive(OpenApi)]
-#[openapi(
-    info(
-        title = "Composition Render API",
-        description = "todo",
-        contact(name = "dyn.art", url = "https://s.dyn.art/github"),
-        version = "1.0.0"
-    ),
-    paths(crate::routes::v1::render::controller::render_composition)
-)]
-pub struct ApiDocs;
+// #[derive(OpenApi)]
+// #[openapi(
+//     info(
+//         title = "Composition Render API",
+//         description = "todo",
+//         contact(name = "dyn.art", url = "https://s.dyn.art/github"),
+//         version = "1.0.0"
+//     ),
+//     paths(crate::routes::v1::render::controller::render_composition)
+// )]
+// pub struct ApiDocs;
 
-impl ApiDocs {
-    pub fn generate() -> String {
-        ApiDocs::openapi().to_json().unwrap()
-    }
-}
+// impl ApiDocs {
+//     pub fn generate() -> String {
+//         ApiDocs::openapi().to_json().unwrap()
+//     }
+// }
