f4349697e8fab74c1068ea73795ad02fb76133cc
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-03-27 09:26:30 +0100

    #develop wip rust api?

--- packages/svg-comp/src/factories.ts
+++ packages/svg-comp/src/factories.ts
@@ -1,6 +1,12 @@
 import { Composition, type TCompositionConfig } from './Composition';
 import { SvgRenderer, type TsvgRendererOptions } from './render';
 
+export function createComposition(config: TCompositionConfig): Composition {
+	const composition = new Composition(config);
+	composition.update();
+	return composition;
+}
+
 export function createSvgComposition(config: TSvgCompositionConfig): Composition {
 	const { renderer: renderOptions = {}, ...compositionConfig } = config;
 	const composition = new Composition(compositionConfig);
