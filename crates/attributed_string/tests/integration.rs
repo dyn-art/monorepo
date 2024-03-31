f140c9e1fd9b79ea7534fe5dc262d5e8fcf30a04
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-03-31 17:13:03 +0200

    #60 added basic text align tests

--- crates/attributed_string/tests/integration.rs
+++ crates/attributed_string/tests/integration.rs
@@ -3,12 +3,13 @@ mod common;
 use common::assert_attributed_string_rendered;
 use dyn_attributed_string::{
     attrs::{Attrs, AttrsInterval},
-    AttributedString, AttributedStringConfig, LineWrap,
+    AttributedString, AttributedStringConfig, HorizontalTextAlignment, LineWrap,
+    VerticalTextAlignment,
 };
-use dyn_fonts_book::font::info::FontFamily;
+use dyn_fonts_book::font::{info::FontFamily, variant::FontWeight};
 use dyn_utils::{
     properties::size::Size,
-    units::{abs::Abs, font_unit::FontUnit},
+    units::{abs::Abs, em::Em, font_unit::FontUnit},
 };
 
 #[test]
@@ -29,7 +30,7 @@ fn test_hebrew_word() {
 
     let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
 
-    assert_attributed_string_rendered("a_hebrew_word", &mut attributed_string);
+    assert_attributed_string_rendered("test_hebrew_word", &mut attributed_string);
 }
 
 #[test]
@@ -51,7 +52,7 @@ fn test_hebrew_paragraph() {
 
     let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
 
-    assert_attributed_string_rendered("a_hebrew_paragraph", &mut attributed_string);
+    assert_attributed_string_rendered("test_hebrew_paragraph", &mut attributed_string);
 }
 
 #[test]
@@ -73,7 +74,10 @@ fn test_english_mixed_with_hebrew_paragraph() {
 
     let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
 
-    assert_attributed_string_rendered("some_english_mixed_with_hebrew", &mut attributed_string);
+    assert_attributed_string_rendered(
+        "test_english_mixed_with_hebrew_paragraph",
+        &mut attributed_string,
+    );
 }
 
 #[test]
@@ -94,7 +98,7 @@ fn test_arabic_word() {
 
     let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
 
-    assert_attributed_string_rendered("an_arabic_word", &mut attributed_string);
+    assert_attributed_string_rendered("test_arabic_word", &mut attributed_string);
 }
 
 #[test]
@@ -116,7 +120,7 @@ fn test_arabic_paragraph() {
 
     let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
 
-    assert_attributed_string_rendered("an_arabic_paragraph", &mut attributed_string);
+    assert_attributed_string_rendered("test_arabic_paragraph", &mut attributed_string);
 }
 
 #[test]
@@ -138,5 +142,316 @@ fn test_english_mixed_with_arabic_paragraph() {
 
     let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
 
-    assert_attributed_string_rendered("some_english_mixed_with_arabic", &mut attributed_string);
+    assert_attributed_string_rendered(
+        "test_english_mixed_with_arabic_paragraph",
+        &mut attributed_string,
+    );
+}
+
+#[test]
+fn test_english_mixed_with_hebrew_paragraph_and_different_fonts() {
+    let text = String::from("Hello, world!\nשלום עולם!\nThis is a mix of English and Hebrew.");
+    let attrs_intervals = vec![
+        AttrsInterval {
+            start: 0,
+            stop: 10,
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Fira Mono")))
+                .font_weight(FontWeight::MEDIUM)
+                .font_size(Abs::pt(48.0))
+                .letter_spacing(FontUnit::em(Em::new(0.5))),
+        },
+        AttrsInterval {
+            start: 10,
+            stop: text.len(),
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Noto Sans")))
+                .font_weight(FontWeight::REGULAR)
+                .font_size(Abs::pt(24.0)),
+        },
+    ];
+    let config = AttributedStringConfig {
+        size: Size::new(Abs::pt(300.0), Abs::pt(240.0)),
+        line_wrap: LineWrap::Word,
+        ..Default::default()
+    };
+
+    let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
+
+    assert_attributed_string_rendered(
+        "test_english_mixed_with_hebrew_paragraph_and_different_fonts",
+        &mut attributed_string,
+    );
+}
+
+#[test]
+fn test_horizontal_text_align_start() {
+    let text = String::from("Hello, world!\nשלום עולם!\nThis is a mix of English and Hebrew.");
+    let attrs_intervals = vec![
+        AttrsInterval {
+            start: 0,
+            stop: 10,
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Fira Mono")))
+                .font_weight(FontWeight::MEDIUM)
+                .font_size(Abs::pt(48.0))
+                .letter_spacing(FontUnit::em(Em::new(0.5))),
+        },
+        AttrsInterval {
+            start: 10,
+            stop: text.len(),
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Noto Sans")))
+                .font_weight(FontWeight::REGULAR)
+                .font_size(Abs::pt(24.0)),
+        },
+    ];
+    let config = AttributedStringConfig {
+        size: Size::new(Abs::pt(300.0), Abs::pt(240.0)),
+        line_wrap: LineWrap::Word,
+        horizontal_text_alignment: HorizontalTextAlignment::Start,
+        ..Default::default()
+    };
+
+    let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
+
+    assert_attributed_string_rendered("test_horizontal_text_align_start", &mut attributed_string);
+}
+
+#[test]
+fn test_horizontal_text_align_end() {
+    let text = String::from("Hello, world!\nשלום עולם!\nThis is a mix of English and Hebrew.");
+    let attrs_intervals = vec![
+        AttrsInterval {
+            start: 0,
+            stop: 10,
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Fira Mono")))
+                .font_weight(FontWeight::MEDIUM)
+                .font_size(Abs::pt(48.0))
+                .letter_spacing(FontUnit::em(Em::new(0.5))),
+        },
+        AttrsInterval {
+            start: 10,
+            stop: text.len(),
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Noto Sans")))
+                .font_weight(FontWeight::REGULAR)
+                .font_size(Abs::pt(24.0)),
+        },
+    ];
+    let config = AttributedStringConfig {
+        size: Size::new(Abs::pt(300.0), Abs::pt(240.0)),
+        line_wrap: LineWrap::Word,
+        horizontal_text_alignment: HorizontalTextAlignment::End,
+        ..Default::default()
+    };
+
+    let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
+
+    assert_attributed_string_rendered("test_horizontal_text_align_end", &mut attributed_string);
+}
+
+#[test]
+fn test_horizontal_text_align_left() {
+    let text = String::from("Hello, world!\nשלום עולם!\nThis is a mix of English and Hebrew.");
+    let attrs_intervals = vec![
+        AttrsInterval {
+            start: 0,
+            stop: 10,
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Fira Mono")))
+                .font_weight(FontWeight::MEDIUM)
+                .font_size(Abs::pt(48.0))
+                .letter_spacing(FontUnit::em(Em::new(0.5))),
+        },
+        AttrsInterval {
+            start: 10,
+            stop: text.len(),
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Noto Sans")))
+                .font_weight(FontWeight::REGULAR)
+                .font_size(Abs::pt(24.0)),
+        },
+    ];
+    let config = AttributedStringConfig {
+        size: Size::new(Abs::pt(300.0), Abs::pt(240.0)),
+        line_wrap: LineWrap::Word,
+        horizontal_text_alignment: HorizontalTextAlignment::Left,
+        ..Default::default()
+    };
+
+    let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
+
+    assert_attributed_string_rendered("test_horizontal_text_align_left", &mut attributed_string);
+}
+
+#[test]
+fn test_horizontal_text_align_right() {
+    let text = String::from("Hello, world!\nשלום עולם!\nThis is a mix of English and Hebrew.");
+    let attrs_intervals = vec![
+        AttrsInterval {
+            start: 0,
+            stop: 10,
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Fira Mono")))
+                .font_weight(FontWeight::MEDIUM)
+                .font_size(Abs::pt(48.0))
+                .letter_spacing(FontUnit::em(Em::new(0.5))),
+        },
+        AttrsInterval {
+            start: 10,
+            stop: text.len(),
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Noto Sans")))
+                .font_weight(FontWeight::REGULAR)
+                .font_size(Abs::pt(24.0)),
+        },
+    ];
+    let config = AttributedStringConfig {
+        size: Size::new(Abs::pt(300.0), Abs::pt(240.0)),
+        line_wrap: LineWrap::Word,
+        horizontal_text_alignment: HorizontalTextAlignment::Right,
+        ..Default::default()
+    };
+
+    let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
+
+    assert_attributed_string_rendered("test_horizontal_text_align_right", &mut attributed_string);
+}
+
+#[test]
+fn test_horizontal_text_align_center() {
+    let text = String::from("Hello, world!\nשלום עולם!\nThis is a mix of English and Hebrew.");
+    let attrs_intervals = vec![
+        AttrsInterval {
+            start: 0,
+            stop: 10,
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Fira Mono")))
+                .font_weight(FontWeight::MEDIUM)
+                .font_size(Abs::pt(48.0))
+                .letter_spacing(FontUnit::em(Em::new(0.5))),
+        },
+        AttrsInterval {
+            start: 10,
+            stop: text.len(),
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Noto Sans")))
+                .font_weight(FontWeight::REGULAR)
+                .font_size(Abs::pt(24.0)),
+        },
+    ];
+    let config = AttributedStringConfig {
+        size: Size::new(Abs::pt(300.0), Abs::pt(240.0)),
+        line_wrap: LineWrap::Word,
+        horizontal_text_alignment: HorizontalTextAlignment::Center,
+        ..Default::default()
+    };
+
+    let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
+
+    assert_attributed_string_rendered("test_horizontal_text_align_center", &mut attributed_string);
+}
+
+#[test]
+fn test_vertical_text_align_top() {
+    let text = String::from("Hello, world!\nשלום עולם!\nThis is a mix of English and Hebrew.");
+    let attrs_intervals = vec![
+        AttrsInterval {
+            start: 0,
+            stop: 10,
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Fira Mono")))
+                .font_weight(FontWeight::MEDIUM)
+                .font_size(Abs::pt(48.0))
+                .letter_spacing(FontUnit::em(Em::new(0.5))),
+        },
+        AttrsInterval {
+            start: 10,
+            stop: text.len(),
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Noto Sans")))
+                .font_weight(FontWeight::REGULAR)
+                .font_size(Abs::pt(24.0)),
+        },
+    ];
+    let config = AttributedStringConfig {
+        size: Size::new(Abs::pt(300.0), Abs::pt(300.0)),
+        line_wrap: LineWrap::Word,
+        vertical_text_alignment: VerticalTextAlignment::Top,
+        ..Default::default()
+    };
+
+    let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
+
+    assert_attributed_string_rendered("test_vertical_text_align_top", &mut attributed_string);
+}
+
+#[test]
+fn test_vertical_text_align_bottom() {
+    let text = String::from("Hello, world!\nשלום עולם!\nThis is a mix of English and Hebrew.");
+    let attrs_intervals = vec![
+        AttrsInterval {
+            start: 0,
+            stop: 10,
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Fira Mono")))
+                .font_weight(FontWeight::MEDIUM)
+                .font_size(Abs::pt(48.0))
+                .letter_spacing(FontUnit::em(Em::new(0.5))),
+        },
+        AttrsInterval {
+            start: 10,
+            stop: text.len(),
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Noto Sans")))
+                .font_weight(FontWeight::REGULAR)
+                .font_size(Abs::pt(24.0)),
+        },
+    ];
+    let config = AttributedStringConfig {
+        size: Size::new(Abs::pt(300.0), Abs::pt(300.0)),
+        line_wrap: LineWrap::Word,
+        vertical_text_alignment: VerticalTextAlignment::Bottom,
+        ..Default::default()
+    };
+
+    let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
+
+    assert_attributed_string_rendered("test_vertical_text_align_bottom", &mut attributed_string);
+}
+
+#[test]
+fn test_vertical_text_align_center() {
+    let text = String::from("Hello, world!\nשלום עולם!\nThis is a mix of English and Hebrew.");
+    let attrs_intervals = vec![
+        AttrsInterval {
+            start: 0,
+            stop: 10,
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Fira Mono")))
+                .font_weight(FontWeight::MEDIUM)
+                .font_size(Abs::pt(48.0))
+                .letter_spacing(FontUnit::em(Em::new(0.5))),
+        },
+        AttrsInterval {
+            start: 10,
+            stop: text.len(),
+            val: Attrs::new()
+                .font_family(FontFamily::Named(String::from("Noto Sans")))
+                .font_weight(FontWeight::REGULAR)
+                .font_size(Abs::pt(24.0)),
+        },
+    ];
+    let config = AttributedStringConfig {
+        size: Size::new(Abs::pt(300.0), Abs::pt(300.0)),
+        line_wrap: LineWrap::Word,
+        vertical_text_alignment: VerticalTextAlignment::Center,
+        ..Default::default()
+    };
+
+    let mut attributed_string = AttributedString::new(text, attrs_intervals, config);
+
+    assert_attributed_string_rendered("test_vertical_text_align_center", &mut attributed_string);
 }
