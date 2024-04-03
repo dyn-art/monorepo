14b59ff5ef0c0e983a98395477dbfc974d7c63ad
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-04-03 07:57:06 +0200

    #64 updated ui library

--- packages/ui/src/components/primitive/DropdownMenu.tsx
+++ packages/ui/src/components/primitive/DropdownMenu.tsx
@@ -1,5 +1,15 @@
-// Imported from shadcn UI
-// https://ui.shadcn.com/docs/components/dropdown-menu
+/**
+ * -----------------------------------------------------------------------------
+ * This file includes code derived from the project shadcn-ui/ui by \@shadcn.
+ * Project Repository: https://github.com/shadcn-ui/ui/blob/main/apps/www/registry/new-york/ui/dropdown-menu.tsx
+ *
+ * Date of Import: 03 April 2024
+ * -----------------------------------------------------------------------------
+ * The code included in this file is licensed under the MIT License,
+ * as per the original project by \@shadcn.
+ * For the license text, see: https://github.com/shadcn-ui/ui/blob/main/LICENSE.md
+ * -----------------------------------------------------------------------------
+ */
 
 'use client';
 
