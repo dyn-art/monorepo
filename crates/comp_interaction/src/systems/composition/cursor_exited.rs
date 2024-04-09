857dcc6e7f5af3944048e7c86adea8f9f693ca3b
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-04-09 07:49:07 +0200

    #64 fixed typo

--- crates/comp_interaction/src/systems/composition/cursor_exited.rs
+++ crates/comp_interaction/src/systems/composition/cursor_exited.rs
@@ -2,7 +2,10 @@ use crate::{
     events::CursorExitedCompInputEvent,
     input::{
         keyboard::KeyCodeButtonInput,
-        mouse::{MouseButtonButtonInputRes, MouseButtonOnEntityButtonInputRes},
+        mouse::{
+            MouseButtonButtonInputRes, MouseButtonOnEntityButtonInputRes,
+            MouseButtonOnResizeHandleButtonInputRes, MouseButtonOnRotateHandleButtonInputRes,
+        },
     },
     resources::comp_interaction::{CompInteractionRes, InteractionMode},
 };
@@ -14,12 +17,16 @@ pub fn cursor_exited_comp_input_system(
     mut keyboard_input_res: ResMut<KeyCodeButtonInput>,
     mut mouse_button_on_comp_input_res: ResMut<MouseButtonButtonInputRes>,
     mut mouse_button_on_entity_input_res: ResMut<MouseButtonOnEntityButtonInputRes>,
+    mut mouse_button_on_resize_handle_input_res: ResMut<MouseButtonOnResizeHandleButtonInputRes>,
+    mut mouse_button_on_rotate_handle_input_res: ResMut<MouseButtonOnRotateHandleButtonInputRes>,
 ) {
     if event_reader.read().len() > 0 {
         log::info!("[cursor_exited_comp_input_system]");
         comp_interaction_res.interaction_mode = InteractionMode::None;
-        keyboard_input_res.reset_all();
+        keyboard_input_res.release_all();
         mouse_button_on_comp_input_res.release_all();
         mouse_button_on_entity_input_res.release_all();
+        mouse_button_on_resize_handle_input_res.release_all();
+        mouse_button_on_rotate_handle_input_res.release_all();
     }
 }
