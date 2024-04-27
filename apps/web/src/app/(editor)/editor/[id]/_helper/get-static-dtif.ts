58b328b5c093c6c8702927a523908c652d2201a0
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-04-27 08:08:36 +0200

    #develop; moved templates into app folder?

--- apps/web/src/app/(editor)/editor/[id]/_helper/get-static-dtif.ts
+++ apps/web/src/app/(editor)/editor/[id]/_helper/get-static-dtif.ts
@@ -1,10 +1,11 @@
 import { promises as fs } from 'node:fs';
 import type { COMP } from '@dyn/dtif-comp';
 
+// https://vercel.com/guides/loading-static-file-nextjs-api-route
 export async function getStaticDtif(id: string): Promise<unknown> {
 	try {
 		return JSON.parse(
-			await fs.readFile(`${process.cwd()}/templates/dtif/${id}.json`, 'utf8')
+			await fs.readFile(`${process.cwd()}/src/app/templates/dtif/${id}.json`, 'utf8')
 		) as COMP.DtifComposition;
 	} catch (e) {
 		return null;
