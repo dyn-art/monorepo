6b87cf756da3d4cfd9af45adb814431c5a3ab284
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-05-13 16:47:01 +0200

    #79 fixed typo

--- crates/comp_core/src/systems/text.rs
+++ crates/comp_core/src/systems/text.rs
@@ -1,6 +1,6 @@
 use bevy_ecs::{
     entity::Entity,
-    query::{Changed, Or, With, Without},
+    query::{Changed, With},
     system::{Commands, Query, ResMut},
 };
 use dyn_attributed_string::{
@@ -23,22 +23,17 @@ use dyn_utils::units::{abs::Abs, auto_length::AutoLength};
 pub fn compute_text_from_scratch(
     mut commands: Commands,
     mut assets_res: ResMut<AssetsRes>,
-    mut query: Query<
-        (Entity, &TextCompNode, &mut SizeMixin),
-        (
-            Or<(Changed<TextCompNode>, Changed<SizeMixin>)>,
-            Without<AttributedStringMixin>,
-        ),
-    >,
+    mut query: Query<(Entity, &TextCompNode, &mut SizeMixin), Changed<TextCompNode>>,
 ) {
     for (entity, text, mut size_mixin) in query.iter_mut() {
-        let mut attributed_string = AttributedString::new(
-            text.text.clone(),
-            text.attributes
-                .iter()
-                .map(|attrs| attrs.to_attrs_interval())
-                .collect(),
-        );
+        let mut intervals = text
+            .attributes
+            .iter()
+            .map(|attrs| attrs.to_attrs_interval())
+            .collect();
+        AttributedString::adjust_intervals(&mut intervals, &text.text);
+
+        let mut attributed_string = AttributedString::new(text.text.clone(), intervals);
 
         attributed_string.tokenize_text(assets_res.get_fonts_book_mut());
         let layout_handler = attributed_string.layout(LayoutConfig {
@@ -101,55 +96,5 @@ pub fn compute_text_on_size_change(
             horizontal_text_alignment: text.horizontal_text_alignment,
             vertical_text_alignment: text.vertical_text_alignment,
         });
-    }
-}
-
-pub fn compute_text_on_node_change(
-    mut assets_res: ResMut<AssetsRes>,
-    mut query: Query<
-        (&TextCompNode, &mut AttributedStringMixin, &mut SizeMixin),
-        (With<AttributedStringMixin>, Changed<TextCompNode>),
-    >,
-) {
-    for (text, mut attributed_string_mixin, mut size_mixin) in query.iter_mut() {
-        let mut new_attributed_string = AttributedString::new(
-            text.text.clone(),
-            text.attributes
-                .iter()
-                .map(|attrs| attrs.to_attrs_interval())
-                .collect(),
-        );
-        new_attributed_string.tokenize_text(assets_res.get_fonts_book_mut());
-        let layout_handler = attributed_string_mixin.0.layout(LayoutConfig {
-            size: match text.sizing_mode {
-                TextSizingMode::Fixed => LayoutSize::new(
-                    AutoLength::abs(size_mixin.0.width),
-                    AutoLength::abs(size_mixin.0.height),
-                ),
-                TextSizingMode::WidthAndHeight => {
-                    LayoutSize::new(AutoLength::Auto, AutoLength::Auto)
-                }
-                TextSizingMode::Height => {
-                    LayoutSize::new(AutoLength::abs(size_mixin.0.width), AutoLength::Auto)
-                }
-            },
-            line_wrap: text.line_wrap,
-            horizontal_text_alignment: text.horizontal_text_alignment,
-            vertical_text_alignment: text.vertical_text_alignment,
-        });
-
-        let text_size = layout_handler.compute_text_size(attributed_string_mixin.0.get_spans());
-
-        // Update bounds
-        if text.sizing_mode == TextSizingMode::WidthAndHeight {
-            size_mixin.0.width = Abs::pt(text_size.width());
-        }
-        if text.sizing_mode == TextSizingMode::WidthAndHeight
-            || text.sizing_mode == TextSizingMode::Height
-        {
-            size_mixin.0.height = Abs::pt(text_size.height());
-        }
-
-        attributed_string_mixin.0 = new_attributed_string;
     }
 }
