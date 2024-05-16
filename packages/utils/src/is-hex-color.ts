c11eb4764e76d22c403d04ff47e9805fbd60340c
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-05-16 18:07:47 +0200

    #79 fixed typo

--- packages/utils/src/is-hex-color.ts
+++ packages/utils/src/is-hex-color.ts
@@ -1,5 +1,5 @@
 import type { THexColor } from './types';
 
 export function isHexColor(color: unknown): color is THexColor {
-	return typeof color === 'string' && /^#(?:[a-f\d]{3}|[a-f\d]{6})$/i.test(color);
+	return typeof color === 'string' && /^#?(?:[a-f0-9]{6}|[a-f0-9]{3})$/i.test(color);
 }
