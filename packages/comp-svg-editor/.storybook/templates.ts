6c25df4eb92d045b56929e22924589633f18c9f1
BennoDev <57860196+bennoinbeta@users.noreply.github.com>
2024-05-23 08:20:39 +0200

    #79 added nested template

--- packages/comp-svg-editor/.storybook/templates.ts
+++ packages/comp-svg-editor/.storybook/templates.ts
@@ -3,6 +3,7 @@ import defaultDtifTemplate from './assets/templates/default.json';
 import squareMdtifTemplate from './assets/templates/m-square.json';
 import tempMdtifTemplate from './assets/templates/m-temp.json';
 import tweetMdtifTemplate from './assets/templates/m-tweet.json';
+import nestedDtifTemplate from './assets/templates/nested.json';
 import sqareDtifTemplate from './assets/templates/square.json';
 import tempDtifTemplate from './assets/templates/temp.json';
 import textDtifTemplate from './assets/templates/text.json';
@@ -14,6 +15,7 @@ export default {
 		sqare: sqareDtifTemplate,
 		text: textDtifTemplate,
 		tweet: tweetDtifTemplate,
+		nested: nestedDtifTemplate,
 		temp: tempDtifTemplate
 	},
 	mdtif: {
