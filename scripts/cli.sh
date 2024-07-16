dc6abaec2d29ed3445b5867745def9138342b265
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-07-16 06:11:29 +0200

    #develop log system environment variables

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -12,8 +12,12 @@ set -e
 
 DIR="$(dirname "$0")"
 
+echo "VERCEL: ${VERCEL}"
+echo "CI: ${CI}"
+echo "VERCEL_ENV: ${VERCEL_ENV}"
+
 # Check if we are running on Vercel (https://vercel.com/docs/projects/environment-variables/system-environment-variables)
-if [ -n "$VERCEL" ]; then
+if [ "$VERCEL" = "1" ]; then # TODO: Doesn't detect vercel env
     echo "🔄 - ⬛️ Detected Vercel environment. Setting up..."
     chmod +x "$DIR/vercel/setup_cli.sh"
     source "$DIR/vercel/setup_cli.sh"
