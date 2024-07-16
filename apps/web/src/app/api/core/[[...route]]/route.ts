b6f2845b5b45b1cde681cc00b5b55fd0de4d742a
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-07-16 09:28:27 +0200

    #develop detached api route because somehow it can't find env variable

--- apps/web/src/app/api/core/[[...route]]/route.ts
+++ apps/web/src/app/api/core/[[...route]]/route.ts
@@ -1,10 +1,8 @@
-import { Hono } from 'hono';
-import { handle } from 'hono/vercel';
-import { createApp } from '@dyn/api-core';
+// import { Hono } from 'hono';
+// import { handle } from 'hono/vercel';
+// import { createApp } from '@dyn/api-core';
 
-// export const runtime = 'edge';
+// const app = createApp(new Hono().basePath('/api'));
 
-const app = createApp(new Hono().basePath('/api'));
-
-export const GET = handle(app);
-export const POST = handle(app);
+// export const GET = handle(app);
+// export const POST = handle(app);
